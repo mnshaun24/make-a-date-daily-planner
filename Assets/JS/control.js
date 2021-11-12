bfafe71a608e3c8dd0d1be44d050e2c1d86ae7b7
Shaun S <shaun.sticka@outlook.com>
2021-11-11 23:25:40 -0600

    started for loop submitted for help

--- Assets/JS/control.js
+++ Assets/JS/control.js
@@ -1,21 +1,44 @@
 // dynamically create day display at top of page
 
-$.get("#currentDay");
 $("#currentDay").append("Hi! This is your schedule for " + moment().format('dddd, MMM Do'));
 
-// create arary for each time block
+// create array for each time block
 var hoursIndex = 0
 
 var hours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
 
-// got the following code from class resource. Not sure how it works or what it's doing. But they suggested I use it.. update: and it broke all of my code. 
-hours.forEach( function(hourString, index, array){
-    console.log("hourString ", hourString)
-    //console.log("index ", index) //comment this line back in whenever.
-    // console.log("array ", array)  //comment this line back in if you like.
+// loop through array to display dynamically
+
+for (let i = 0; i < hours.length; i++) {
+    var hoursDisplay = hours[i];
+
+    // create hours and append
+    var newHour = $("<div>").addClass("time-block row row-1 hour").text(hours[i]);
+    $(".container").append(newHour);
+
+    // create and append text area
+    var newText = $("<textarea>").addClass("time-block row description col-10")
+    $("newHour").append(newText);
+
+    // create and append button
+    var newButton = $("<button>").addClass("time-block row saveBtn col-1").attr("width", "20");
+    $("newHour").append(newButton);
+}
+
+
+
+
+
+
+
+// // got the following code from class resource. Not sure how it works or what it's doing. But they suggested I use it.. update: and it broke all of my code. 
+// hours.forEach( function(hourString, index, array){
+//     console.log("hourString ", hourString)
+//     //console.log("index ", index) //comment this line back in whenever.
+//     // console.log("array ", array)  //comment this line back in if you like.
   
-    var newElem = $("p") //make a p-tag
-    newElem.text(hourString) //give it some text (from hours array)
-    $(".container").append(newElem) //throw that somewhere on the page.
-    //all of this should be edited to make nice rows, textareas, and buttons of course.
-  });
+//     var newElem = $("p") //make a p-tag
+//     newElem.text(hourString) //give it some text (from hours array)
+//     $(".container").append(newElem) //throw that somewhere on the page.
+//     //all of this should be edited to make nice rows, textareas, and buttons of course.
+//   });
