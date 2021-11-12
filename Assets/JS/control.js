b7a859523471fd0883deb8c40c5973a2e2e14751
Shaun S <shaun.sticka@outlook.com>
2021-11-11 21:22:40 -0600

    committed for more help, almost nothing has changed since last

--- Assets/JS/control.js
+++ Assets/JS/control.js
@@ -8,13 +8,14 @@ var hoursIndex = 0
 
 var hours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
 
-
-// create code to append time block
-// $.get(".container");
-// $(".container").append(hours[0]);
-
-// create loop to display all hours from index
-$.each(hours, function() {
-    $.get(".container");
-    $(".container").append(hours[i])
-});
+// got the following code from class resource. Not sure how it works or what it's doing. But they suggested I use it.. update: and it broke all of my code. 
+hours.forEach( function(hourString, index, array){
+    console.log("hourString ", hourString)
+    //console.log("index ", index) //comment this line back in whenever.
+    // console.log("array ", array)  //comment this line back in if you like.
+  
+    var newElem = $("p") //make a p-tag
+    newElem.text(hourString) //give it some text (from hours array)
+    $(".container").append(newElem) //throw that somewhere on the page.
+    //all of this should be edited to make nice rows, textareas, and buttons of course.
+  });
